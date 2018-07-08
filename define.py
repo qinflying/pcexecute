4010ccd3f39ee56cb70d7cb9460c9c1abeab0ed1
wushun <qinflying@outlook.com>
2018-07-08 18:25:41 +0800

    add chinese font
    Signed-off-by: wushun <qinflying@outlook.com>

---
+++ define.py
@@ -0,0 +1,9 @@
+#-*- coding:utf-8 -*-
+import kivy
+#宏定义
+
+#字体
+FONT_KAI = kivy.resources.resource_find("resource/font/simkai.ttf")
+FONT_HEI = kivy.resources.resource_find("resource/font/simhei.ttf")
+
+COMMON_FONT = FONT_KAI
